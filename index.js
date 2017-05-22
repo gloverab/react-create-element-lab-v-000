095035bb56fa305c0e4247e35e54de222dcc1041
Alex Glover <Alex@MacBook-Pro-3.local>
2017-05-22 14:41:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 //Code React element here
+const title = React.createElement('h1', {}, 'An Awesome Person')
+const para = React.createElement('p', {}, 'Who is learning React')
+const list = React.createElement('ul', {className: 'me_interests'},
+[
+  React.createElement('li', {}, 'JavaScript'),
+  React.createElement('li', {}, 'React'),
+  React.createElement('li', {}, 'Movies'),
+  React.createElement('li', {}, 'Ice cream')
+])
+
+const meInReact = React.createElement('div', {className: "me"}, [title, para, list])
+
+ReactDOM.render(meInReact, document.getElementById("main"))
